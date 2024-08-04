0eee46639712bb95ae3f2d88911585254dd229fb
REGEX777 <hmmho705@gmail.com>
2024-08-04 20:21:59 +0530

    Fixed the post page and the index page

--- routes/Home.js
+++ routes/Home.js
@@ -1,10 +1,16 @@
 // Keydetails.js
 import express from 'express';
 
+// Model import
+
+import Post from '../models/Post.js'
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render('index')
+    Post.find({}).then((posts)=>{
+        res.render('index', {posts: posts})
+    }).catch(err=>console.log(err))
 });
 
 export default router;
--- routes/postPage.js
+++ routes/postPage.js
@@ -1,9 +1,17 @@
 import express from 'express';
 
+// Model import
+import Post from '../models/Post.js'
+
 const router = express.Router();
 
-router.get("/", (req, res)=>{
-    res.render('postPage')
+router.get("/:filename", async(req, res)=>{
+    const post = await Post.findOne({fileName: req.params.filename})
+    if(!post){
+        res.json({"Error":"Post not found"})
+    }
+
+    res.render('postPage', {post: post})
 })
 
 export default router;
