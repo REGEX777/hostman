b532ea3add7b2cb5e3416dff406eff3b0fe085f9
REGEX777 <hmmho705@gmail.com>
2024-08-07 20:08:01 +0530

    Updated the account.ejs for some bug fixes

--- routes/Account.js
+++ routes/Account.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import multer from 'multer';
 import path from 'path';
-import User from '../models/User.js'; 
+import User from '../models/User.js';
 import ApiKeyLog from '../models/ApiKeyLog.js'; // Assuming you have a model for logging API key usage
 import { ensureAuthenticated } from '../middleware/auth.js';
 import { v4 as uuidv4 } from 'uuid';
