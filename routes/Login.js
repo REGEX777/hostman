049fb7b8c0488b154f45321cefcacfbff7a64720
REGEX777 <hmmho705@gmail.com>
2024-08-10 20:16:34 +0530

    Added a loggin system

--- routes/Login.js
+++ routes/Login.js
@@ -3,20 +3,38 @@ import passport from 'passport';
 import rateLimit from 'express-rate-limit';
 
 const router = express.Router();
+
+const failedLoginAttempts = {};
+
 const loginLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, 
-    max: 5, 
+    windowMs: 15 * 60 * 1000,
+    max: 5,
     message: 'Too many login attempts from this IP, please try again after 15 minutes',
+    skip: (req) => {
+        const email = req.body.email;
+        return !failedLoginAttempts[email] || failedLoginAttempts[email] < 3;
+    }
 });
 
 router.get('/', (req, res) => {
     res.render('login');
 });
 
-router.post('/',loginLimiter, passport.authenticate('local', {
-    successRedirect: '/', 
-    failureRedirect: '/login', 
-    failureFlash: true 
-}));
+router.post('/', loginLimiter, (req, res, next) => {
+    const email = req.body.email;
+    passport.authenticate('local', (err, user, info) => {
+        if (err) return next(err);
+        if (!user) {
+            failedLoginAttempts[email] = (failedLoginAttempts[email] || 0) + 1;
+            req.flash('error', info.message);
+            return res.redirect('/login');
+        }
+        req.logIn(user, (err) => {
+            if (err) return next(err);
+            delete failedLoginAttempts[email];
+            return res.redirect('/');
+        });
+    })(req, res, next);
+});
 
 export default router;
