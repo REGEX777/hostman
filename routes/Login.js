94a7c9da062487465c9251174aa81126c32a8111
REGEX777 <hmmho705@gmail.com>
2024-08-10 18:32:23 +0530

    security stuff

--- routes/Login.js
+++ routes/Login.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import passport from 'passport';
+import rateLimit from 'express-rate-limit';
 
 const router = express.Router();
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 5, 
+    message: 'Too many login attempts from this IP, please try again after 15 minutes',
+});
 
 router.get('/', (req, res) => {
     res.render('login');
 });
 
-router.post('/', passport.authenticate('local', {
+router.post('/',loginLimiter, passport.authenticate('local', {
     successRedirect: '/', 
     failureRedirect: '/login', 
     failureFlash: true 
